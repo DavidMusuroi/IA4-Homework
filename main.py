60cb02877dbc1bea629834bf89b3bfa885c608e7
DaMiAnThEk <damiantanasa10022005@gmail.com>
2025-10-30 03:36:41 +0200

    Changing colour main menu buttons

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import pygame, sys
-from main_menu import main_menu
+from main_menu import display_main_menu
 from get_font import get_font
 
 pygame.init()
 
-menu, img_menu = main_menu()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Starfield Assault")
 
 while True:
-    menu.blit(img_menu, (0, 0))
-    menu_text = get_font(120).render("MAIN MENU", True, "#5240b6")
-    menu_rect = menu_text.get_rect(center = (640, 100))
-
-    menu.blit(menu_text, menu_rect)
+    mouse_pos = pygame.mouse.get_pos()
+    display_main_menu(screen, mouse_pos)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
