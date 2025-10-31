d0a82766170c29bc1029b5b36dd54af0ea35c92b
DaMiAnThEk <damiantanasa10022005@gmail.com>
2025-10-31 17:01:16 +0200

    Devided Menu loop and Game loop

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Starfield Assault")
+clock = pygame.time.Clock()
+menu_is_over = False
 
-while True:
+#Menu loop
+while menu_is_over == False:
     mouse_pos = pygame.mouse.get_pos()
     display_main_menu(screen, mouse_pos)
 
@@ -15,5 +18,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            menu_is_over = True
+            
+    clock.tick(60) #limit FPS to 60
+    pygame.display.update()
 
+#Game loop
+while True:
+    img_menu = pygame.image.load('assets/main_menu.png')
+    screen.blit(img_menu, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    clock.tick(60) #limit FPS to 60
     pygame.display.update()
